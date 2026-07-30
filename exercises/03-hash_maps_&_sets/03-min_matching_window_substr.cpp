52d7333b4d1eea051400e95e1c07233cddbd88f5
Bert Chun Chang <b.c.chang@outlook.com>
2026-07-30 15:50:46 +0800

    feat: add min matching window substring solution

---
+++ exercises/03-hash_maps_&_sets/03-min_matching_window_substr.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
+using std::string;
+using std::unordered_map;
+
+string findMinMatchingWindowSubstr(const string& s, const string& t) {
+    unordered_map<char, int> need;
+    for (char c : t) need[c]++;
+
+    int have = 0;
+    int required = (int)need.size();
+    int left = 0;
+    int bestStart = 0;
+    int bestLen = INT_MAX;
+    unordered_map<char, int> window;
+
+    for (int right = 0; right < (int)s.size(); right++) {
+        char c = s[right];
+        window[c]++;
+        if (need.count(c) && need[c] == window[c]) {
+            have++;
+        }
+        while (have == required) {
+            char lc = s[left];
+            window[lc]--;
+            if (right - left + 1 < bestLen) {
+                bestLen = right - left + 1;
+                bestStart = left;
+            }
+            if (need.count(lc) && window[lc] < need[lc]) {
+                have--;
+            }
+            left++;
+        }
+    }
+
+    return bestLen == INT_MAX ? "" : s.substr(bestStart, bestLen);
+}
+
+int main() {
+    std::cout << findMinMatchingWindowSubstr("ADOBECODEBANC", "ABC") << "\n";
+    std::cout << findMinMatchingWindowSubstr("a", "a") << "\n";
+    std::cout << findMinMatchingWindowSubstr("a", "b") << "\n";
+    std::cout << findMinMatchingWindowSubstr("aa", "aa") << "\n";
+}
