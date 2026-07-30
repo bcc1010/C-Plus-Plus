f1d84f42453b870b7f627829a795f2689f4b3ae4
Bert Chun Chang <b.c.chang@outlook.com>
2026-07-30 15:09:01 +0800

    fix: add size short circuit check

--- exercises/03-hash_maps_&_sets/01-check_anagram.cpp
+++ exercises/03-hash_maps_&_sets/01-check_anagram.cpp
@@ -5,6 +5,7 @@ using std::string;
 using std::vector;
 
 bool checkAnagram(const string& s, const string& t) {
+    if (s.size() == t.size()) return false;
     vector<int> freq(26);
     for (char c : s) freq[c - 'a']++;
     for (char c : t) freq[c - 'a']--;
