a4279b1fd24180923f549db30d3ea2716c9f3570
Bert Chun Chang <b.c.chang@outlook.com>
2026-07-31 22:39:54 +0800

    feat: add is subsequence prac soln

---
+++ tutorial/04-Check_subsequence.cpp
@@ -0,0 +1,14 @@
+// Is s a subsequence of t?
+// "ace" is a subsequence of "abcde" - a, c, e appear in order
+bool isSubsequence(const std::string& s, const std::string& t) {
+    int i = 0, j = 0;
+
+    while (i < (int)s.size() && j < (int)t.size()) {
+        if (s[i] == t[j]) {
+            i++;
+        }
+        j++;
+    }
+
+    return i = s.size();
+}
